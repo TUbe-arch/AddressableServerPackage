e6ee596dab29320fcc83d510236883b396638970
TUbe <tony981123@gmail.com>
2026-05-27 19:18:25 +0800

    上傳批次動態載入、四台攝影機控制系統、攝影機開關機簡易回饋、並且新增了點擊機櫃移動、優化後端bundle包大小

--- WebGL_View/ServiceWorker.js
+++ WebGL_View/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "TCIT-DCIM_NCHC-0.2.3";
+const cacheName = "TCIT-DCIM_NCHC-0.2.4";
 const contentToCache = [
     "TemplateData/style.css"
     // 注意：Unity build 檔案不放進 contentToCache
